c5e2eb1f150a92102d9d3b07aecf6d29fa5b42dd
shhanifff <shanifsha12356@gmail.com>
2025-01-18 15:15:24 +0530

    Time: 7 ms (7.32%), Space: 60.3 MB (5.19%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -3,45 +3,45 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function (nums) {
-   let a = [];  
-let bb = []; 
+    let a = [];
+    let bb = [];
 
 
-for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === 1) {
-        a.push(1);  
-    } else {
-        if (a.length > 0) {  
-            bb.push(a);  
-            a = [];  
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            a.push(1);
+        } else {
+            if (a.length > 0) {
+                bb.push(a);
+                a = [];
+            }
         }
     }
-}
 
 
-if (a.length > 0) {
-    bb.push(a);
-}
-
-let count = [];
-
-
-for (let i = 0; i < bb.length; i++) {
-    let c = bb[i];
-    let aa = 0;
-    for (let j = 0; j < c.length; j++) {
-        aa++;
+    if (a.length > 0) {
+        bb.push(a);
     }
-    count.push(aa);
-}
+
+    let count = [];
 
 
-if (count.length === 0) {
-    return 0;  
-}
+    for (let i = 0; i < bb.length; i++) {
+        let c = bb[i];
+        let aa = 0;
+        for (let j = 0; j < c.length; j++) {
+            aa++;
+        }
+        count.push(aa);
+    }
 
 
-return Math.max(...count);
+    if (count.length === 0) {
+        return 0;
+    }
+
+
+    return Math.max(...count);
 
 
 };
