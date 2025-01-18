b734cfc45af4387e80a0d060709b0544bf567222
shhanifff <shanifsha12356@gmail.com>
2025-01-18 15:08:12 +0530

    Time: 5 ms (22.04%), Space: 63.7 MB (5.5%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -5,18 +5,18 @@
  */
 var rotate = function (nums, k) {
 
-let n = nums.length;
-    
-   
-    k = k % n;  
-    
-   
-    let part1 = nums.slice(n - k); 
+    let n = nums.length;
+
+
+    k = k % n;
+
+
+    let part1 = nums.slice(n - k);
     let part2 = nums.slice(0, n - k);
-    
-    
-    nums.length = 0; 
-    nums.push(...part1, ...part2); 
+
+
+    nums.length = 0;
+    nums.push(...part1, ...part2);
     return nums
 
 };
