ce3e13d1e363149501e2a3785295561314d51fc1
shhanifff <shanifsha12356@gmail.com>
2025-01-18 12:10:41 +0530

    Time: 5 ms (19.57%), Space: 58.3 MB (5.19%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+   let a = [];  
+let bb = []; 
+
+
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+        a.push(1);  
+    } else {
+        if (a.length > 0) {  
+            bb.push(a);  
+            a = [];  
+        }
+    }
+}
+
+
+if (a.length > 0) {
+    bb.push(a);
+}
+
+let count = [];
+
+
+for (let i = 0; i < bb.length; i++) {
+    let c = bb[i];
+    let aa = 0;
+    for (let j = 0; j < c.length; j++) {
+        aa++;
+    }
+    count.push(aa);
+}
+
+
+if (count.length === 0) {
+    return 0;  
+}
+
+
+return Math.max(...count);
+
+
+};
