a6a43a0304dd2818ba9f6ad123913ea765683f8f
shhanifff <shanifsha12356@gmail.com>
2025-01-18 15:10:21 +0530

    Time: 9 ms (20.46%), Space: 54.5 MB (50.7%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let zeroCount=0
+    for(i=0;i<nums.length;i++){
+        if(nums[i]===0){
+            nums.splice(i,1)
+            zeroCount++
+            i--
+        }
+    }
+   
+    for(i=0;i<zeroCount; i++){
+        nums.push(0)
+    }
+
+    return nums
+
+};
