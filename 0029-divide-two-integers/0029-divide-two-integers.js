44ddd7f1a5efa3b59a34ca30bc308613d92ec572
shhanifff <shanifsha12356@gmail.com>
2025-03-17 09:32:12 +0530

    Time: 0 ms (100%), Space: 55.9 MB (21.05%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function (dividend, divisor) {
+    let result = Math.trunc(dividend / divisor);
+
+    if (result > 2147483647) return 2147483647;
+    if (result < -2147483648) return -2147483648;
+
+    return result
+};
